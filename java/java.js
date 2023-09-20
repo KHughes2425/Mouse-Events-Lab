d4b06f14d3b8319d4425af61fd910b93daf31608
Kim <khughes949@insite.4cd.edu>
2023-09-19 21:12:30 -0700

    errors errors errors

--- java/java.js
+++ java/java.js
@@ -18,6 +18,6 @@ demo.addEventListener('mousedown', (e) => {
 var dot = document.getElementById('dot');
         document.body.addEventListener('click', (e) => {
             dot.style.transform = `translateY(${e.clientY - 25}px)`;
-            dot.style.transform +  `translateX(${e.clientX - 25}px)`;
+            dot.style.transform += `translateX(${e.clientX - 25}px)`;
             dot.style.opacity = 1;
         }, false);
