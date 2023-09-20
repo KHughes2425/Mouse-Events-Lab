aff2ecb7d695a45cd7412ba79486fdcb3816fd14
Kim <khughes949@insite.4cd.edu>
2023-09-20 14:29:43 -0700

    Added a bunch of changes including javascr.

--- java/java.js
+++ java/java.js
@@ -1,23 +1,9 @@
+const div = document.querySelector("#myDiv");
 
+div.style.backgroundcolor = "grey";
+div.style.width = "200px";
+div.style.height = "200px";
 
-const demo  = document.querySelector('.demo');
-const point = document.querySelector('#point');
-
-
-
-demo.addEventListener('mousedown', (e) => {
-    point.innerHTML = e.offsetX + ', ' + e.offsetY;
-    
-    //open the JS console to see the output
-    console.log("Client: " , e.clientX , e.clientY)
-    console.log("Page: " , e.pageX , e.pageY)
-    console.log("Screen: " , e.screenX , e.screenY)
-    console.log("Event Object: ", e);
-});
-
-var dot = document.getElementById('dot');
-        document.body.addEventListener('click', (e) => {
-            dot.style.transform = `translateY(${e.clientY - 25}px)`;
-            dot.style.transform += `translateX(${e.clientX - 25}px)`;
-            dot.style.opacity = 1;
-        }, false);
+div.addEventListener("click", e=> {
+    console.log("You clicked the mouse!");
+})
