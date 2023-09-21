a4f0cca1fe4340208692566dfb6f968053c73055
Kim <khughes949@insite.4cd.edu>
2023-09-21 00:05:05 -0700

    removed header

--- java/java.js
+++ java/java.js
@@ -1,9 +1,21 @@
-const div = document.querySelector("#myDiv");
+const dot = document.getElementById('dot');
+document.body.addEventListener('click', (e) => {
+    dot.style.transform = `translateY(${e.clientY - 25}px)`;
+    dot.style.transform += `translateX(${e.clientX - 25}px)`;
+    dot.style.opacity = 1;
+}, false);
 
-div.style.backgroundcolor = "grey";
-div.style.width = "200px";
-div.style.height = "200px";
+//create shortcut constants
+const demo  = document.querySelector('.demo');
+const point = document.querySelector('#point');
 
-div.addEventListener("click", e=> {
-    console.log("You clicked the mouse!");
-})
+//detect mousedown events and handle them.
+demo.addEventListener('mousedown', (e) => {
+point.innerHTML = e.offsetX + ', ' + e.offsetY;
+
+//open the JS console to see the output
+console.log("Client: " , e.clientX , e.clientY)
+console.log("Page: " , e.pageX , e.pageY)
+console.log("Screen: " , e.screenX , e.screenY)
+console.log("Event Object: ", e);
+});
