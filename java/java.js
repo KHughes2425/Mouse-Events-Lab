34406f1b09fee99342c12bf70d91d23b003d7a5d
Kim <khughes949@insite.4cd.edu>
2023-09-21 20:06:53 -0700

    final  product

--- java/java.js
+++ java/java.js
@@ -1,17 +1,21 @@
-const dot = document.getElementById('dot');
-document.body.addEventListener('click', (e) => {
-    dot.style.transform = `translateY(${e.clientY - 25}px)`;
-    dot.style.transform += `translateX(${e.clientX - 25}px)`;
-    dot.style.opacity = 1;
-}, false);
+var dot = document.getElementById('dot');
+        document.body.addEventListener('click', (e) => {
+            dot.style.transform = `translateY(${e.clientY - 25}px)`;
+            dot.style.transform += `translateX(${e.clientX - 25}px)`;
+            dot.style.opacity = 1;
+        }, false);
 
+
+       
 //create shortcut constants
 const demo  = document.querySelector('.demo');
 const point = document.querySelector('#point');
 
+
 //detect mousedown events and handle them.
 demo.addEventListener('mousedown', (e) => {
 point.innerHTML = e.offsetX + ', ' + e.offsetY;
+
 
 //open the JS console to see the output
 console.log("Client: " , e.clientX , e.clientY)
@@ -19,3 +23,4 @@ console.log("Page: " , e.pageX , e.pageY)
 console.log("Screen: " , e.screenX , e.screenY)
 console.log("Event Object: ", e);
 });
+
